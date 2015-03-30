73cd6bef3cf4fa8ad523d80f4eacb105381e104a
Edsolat <edouard.sabourdy@laposte.net>
2015-03-30 14:21:32 +0200

    second commit

--- grails-app/services/friendsofmine/ActiviteService.groovy
+++ grails-app/services/friendsofmine/ActiviteService.groovy
@@ -8,10 +8,14 @@ class ActiviteService {
     boolean transactional = true
 
     Activite insertOrUpdateActiviteForResponsable(Activite uneActivite, Utilisateur unResponsable) {
-        if (unResponsable.activites.contains(uneActivite))
-            unResponsable.activites.remove(uneActivite)
-        def resultActivite = new Activite(titre : uneActivite.titre, Utilisateur : unResponsable)
-        resultActivite.save()
-        resultActivite
+        //if (!unResponsable.activites && unResponsable.activites.contains(uneActivite))
+        //    unResponsable.activites.remove(uneActivite)
+        uneActivite.setResponsable(unResponsable)
+        unResponsable.addToActivites(uneActivite)
+        unResponsable.save()
+        uneActivite.save()
+        uneActivite
     }
+
+
 }
